6af28a816da6fbb90a26675f9c2ae2c133c4fc6f
oz <oz.rnds@gmail.com>
2024-01-01 21:07:04 +0000

    feat(logic/service,-db/service): Create jobs for media without the job. Implement the insight calculation and closing of the job

--- src/db/service.py
+++ src/db/service.py
@@ -15,7 +15,7 @@ class MediaDBService:
         self.db_service_url = f"http://{host}:{str(port)}"
 
 
-    def get_images_to_analyze(self, engine_name: str, batch_size: int | None = None) -> search.SearchResult:
+    def get_media_to_analyze(self, engine_name: str, batch_size: int | None = None) -> search.SearchResult:
         batch_size = batch_size if batch_size else self.default_batch_size
         
         get_images_api_url = self.db_service_url + f"/v2/no-jobs/media/{engine_name}"
@@ -39,7 +39,57 @@ class MediaDBService:
 
         if results.status_code == 200:
             return search.SearchResult(**results.json())
+        raise Exception(f"{results.json().detail}")
+
+    def get_media_by_ids(self, media_ids_list: str) -> search.SearchResult:
+        
+        get_images_api_url = self.db_service_url + f"/v1/media/search"
+
+        query_params = {
+            "media_id": [media_ids_list]
+        }
+
+        s = requests.Session()
+
+        retries = Retry(total=5,
+                backoff_factor=1,
+                status_forcelist=[429, 500, 502, 503, 504])
+
+        s.mount('http://', HTTPAdapter(max_retries=retries))
+
+        results = s.get(get_images_api_url, params=query_params)
+
+        s.close()
+
+        if results.status_code == 200:
+            return search.SearchResult(**results.json())
         return []
+
+    def get_pending_jobs(self, engine_id: str, batch_size: int | None = None) -> search.SearchResult:
+        batch_size = batch_size if batch_size else self.default_batch_size
+
+        get_jobs_api_url = self.db_service_url + f"/v2/jobs/search"
+
+        params = {
+            "insight_engine_id": engine_id,
+            "status": jobs.InsightJobStatus.PENDING.value
+        }
+
+        s = requests.Session()
+
+        retries = Retry(total=5,
+        backoff_factor=1,
+        status_forcelist=[429, 500, 502, 503, 504])
+
+        s.mount('http://', HTTPAdapter(max_retries=retries))
+
+        results = s.get(get_jobs_api_url, params=params)
+
+        s.close()
+
+        if results.status_code == 200:
+            return search.SearchResult(**results.json())
+
 
     def put_jobs(self, job_list: List[jobs.InsightJob]):
         json = [item.model_dump() for item in job_list]
@@ -55,6 +105,8 @@ class MediaDBService:
         s.mount('http://', HTTPAdapter(max_retries=retries))
 
         results = s.put(put_job_api_url, json=json)
+
+        s.close()
 
         if results.status_code==200:
             return len(results.json())
@@ -75,6 +127,8 @@ class MediaDBService:
 
         results = s.post(update_job_api_url, json=json)
 
+        s.close()
+
         if results.status_code==200:
             return len(results.json())
 
@@ -93,6 +147,8 @@ class MediaDBService:
         s.mount('http://', HTTPAdapter(max_retries=retries))
 
         results = s.put(put_insights_api_url, json=json)
+
+        s.close()
 
         if results.status_code==200:
             return len(results.json())
--- src/logic/service.py
+++ src/logic/service.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
 
@@ -13,7 +14,7 @@ class MonthsEngineLogics:
                  media_db_service: MediaDBService,
                  engine_details: insights.InsightEngine,
                  batch_process_size: int = 200,
-                 batch_processing_period_minutes: int = 120,) -> None:
+                 batch_processing_period_minutes: float = 120,) -> None:
         self.engine = engine_details
         self.media_db_service = media_db_service
         self.batch_process_size = batch_process_size
@@ -27,28 +28,52 @@ class MonthsEngineLogics:
         pass
 
 
+    def create_jobs(self):
+        media_to_process: search.SearchResult = self.media_db_service.get_media_to_analyze(engine_name="months", batch_size=self.batch_process_size)
+        job_list: List[jobs.InsightJob] = []
+        for result in media_to_process.results:
+            media_item = media.MediaStorage(**result)
+            temp_job = jobs.InsightJob(insight_engine_id=self.engine.id,
+                                        media_id=media_item.media_id)
+            logger.info(f"Create new job ({temp_job.id}) for media ({media_item.media_id})")
+            job_list.append(temp_job)
+        if len(job_list)>0:
+            self.media_db_service.put_jobs(job_list)
+
+
     def listen(self):
         while True:
-            media_to_process: search.SearchResult = self.media_db_service.get_images_to_analyze(engine_name="months", batch_size=self.batch_process_size)
+            try:
+                self.create_jobs()
+            except Exception as err:
+                logger.warning(f"Failed to create jobs: {str(err)}")
+            logger.info("Search jobs")
+            jobs_to_process: search.SearchResult = self.media_db_service.get_pending_jobs(engine_id=self.engine.id, batch_size=self.batch_process_size)
+            jobs_to_process: List[jobs.InsightJob] = [jobs.InsightJob(**item) for item in jobs_to_process.results]
+            media_to_process: search.SearchResult = self.media_db_service.get_media_by_ids(media_ids_list=[item.media_id for item in jobs_to_process])
+            media_to_process: List[jobs.InsightJob] = [media.MediaIDs(**media_item) for media_item in media_to_process.results]
+            analizing_dictionary = {}
+            for job in jobs_to_process:
+                analizing_dictionary[job.id] = (job,[media_item for media_item in media_to_process if media_item.media_id==job.media_id][0])
+            
             insights_list = []
-            job_list: List[jobs.InsightJob] = []
-            for result in media_to_process.results:
-                media_item = media.MediaStorage(**result)
-                temp_job = jobs.InsightJob(insight_engine_id=self.engine.id,
-                                           media_id=media_item.media_id)
-                job_list.append(temp_job)
+            for job_id,value in analizing_dictionary.items():
+                temp_job,media_item = value
                 temp_image_insight = insights.Insight(insight_engine_id=self.engine.id,
                                                       media_id=media_item.media_id,
                                                       name=media_item.created_on.strftime("%m-%Y"),
                                                       job_id=temp_job.id,
                                                       status=insights.InsightStatusEnum.APPROVED)
+                logger.info(f"Process job ({job_id}). Added insight calculated: {temp_image_insight.name}")
                 insights_list.append(temp_image_insight)
-            if len(job_list)>0:
-                self.media_db_service.put_jobs(job_list)
+            if len(insights_list)>0:
                 self.media_db_service.put_insights(insights_list)
-                for job in job_list:
+            if len(jobs_to_process)>0:
+                for job in jobs_to_process:
                     job.status = jobs.InsightJobStatus.DONE
-                self.media_db_service.update_jobs(job_list)
+                    job.end_time = datetime.now()
+                if not self.media_db_service.update_jobs(jobs_to_process)>0:
+                    logger.error(f"Could not update job {jobs_to_process}")
             time.sleep(self.batch_processing_period_minutes*60)
 
 
