83a9dc14479342e17192c851e3a4f100e78142a5
oz <oz.rnds@gmail.com>
2024-01-07 14:33:23 +0000

    build(requirements,.vscode,devcontainer,.autodevops): Add pydantic and pydanic-settings. Update the local environment files and build configuration for development and production

--- .autodevops/quick_build_deploy.sh
+++ .autodevops/quick_build_deploy.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 ENVIRONMENT=$1
 
-echo $ENVIRONMENT
+echo Start CD process to $ENVIRONMENT environment
 
 source .autodevops/service.properties
 
-# cz bump -ch
+cz bump -ch
 
 SERVICE_VERSION=$(cz version -p)
-IMAGE_NAME=shkedia-photo-${COMPONENT_NAME}-service:${SERVICE_VERSION}
+IMAGE_NAME=${COMPONENT_NAME}-service:${SERVICE_VERSION}
 IMAGE_FULL_NAME=public.ecr.aws/q2n5r5e8/project-shkedia/${IMAGE_NAME}
 
+echo GET PIP CREDENTIALS
+bash .local/pip/pip_credentials.sh
+
 echo BUILD IMAGE $IMAGE_FULL_NAME
-# docker build . -t ${IMAGE_FULL_NAME}
+docker build . -t ${IMAGE_FULL_NAME}
 
-ENVIRONMENT=dev
-DEPLOYMENT_ENV=.local/${COMPONENT_NAME}_service_${ENVIRONMENT}.env
+# ENVIRONMENT=dev
+# DEPLOYMENT_ENV=.local/${COMPONENT_NAME}_service_${ENVIRONMENT}.env
 
-export ${COMPONENT_NAME^^}_SERVICE_VERSION=$SERVICE_VERSION
+# export ${COMPONENT_NAME^^}_SERVICE_VERSION=$SERVICE_VERSION
 
-docker compose --env-file ${DEPLOYMENT_ENV} up -d
+# docker compose --env-file ${DEPLOYMENT_ENV} up -d
