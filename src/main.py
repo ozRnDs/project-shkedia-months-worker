f670f96efac30d6cd55419c5dec2848f7424e9d8
oz <oz.rnds@gmail.com>
2024-01-07 19:42:13 +0000

    feat(main): Bind consumer and logic to create queue based worker

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import traceback
 import time
 import logging
+import boto3
 logger = logging.getLogger(__name__)
 
 from project_shkedia_models.insights import InsightEngine
@@ -9,6 +10,10 @@ from config import app_config, secret_config
 from db.service import MediaDBService
 from logic.service import MonthsEngineLogics
 from db.user_service import UserDBService, UserRequest
+from publisher.service import PublisherService
+from consumer.service import ConsumerService
+from publisher.service import MonthsTopicsEnum
+from publisher.sns_wrapper import SnsWrapper
 
 auth_service = UserDBService(host=app_config.USER_DB_HOST,
                              port=app_config.USER_DB_PORT,
@@ -24,9 +29,22 @@ month_engine_logics = MonthsEngineLogics(media_db_service=media_service,
                                          batch_process_size=app_config.BATCH_SIZE,
                                          batch_processing_period_minutes=app_config.BATCH_PROCESS_PERIOD_MIN)
 
+consumer_service = ConsumerService(queue_name=app_config.ENGINE_DETAILS.input_queue_name,
+                                   listening_time_seconds=app_config.LISTENING_TIME_SECONDS,
+                                   message_ownership_time_seconds=app_config.MESSAGE_OWNERSHIP_TIME_SECONDS,
+                                   batch_size=app_config.BATCH_SIZE,
+                                   sns_wrapper=SnsWrapper(boto3.resource("sns")),
+                                   environment=app_config.ENVIRONMENT)
+
+consumer_service.bind_topics([app_config.ENGINE_DETAILS.input_source])
+
+consumer_service.add_messages_callback(month_engine_logics.process_messages)
+
+
 if __name__ == "__main__":
     try:
-        month_engine_logics.listen()
+        consumer_service.listen(5)
+        # month_engine_logics.listen()
     except Exception as err:
         logger.error(traceback.format_exc())
 
